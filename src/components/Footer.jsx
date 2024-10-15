a3a2f44d7470a5cb3103f096729ecff0137205f9
syed zohaib <zohaibsyed914@gmail.com>
2024-10-16 00:42:01 +0500

    working

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <>
+            <div className="footerContainer">
+                <p className='footerPara'>We are open until 22:00. Come visit us or order online</p>
+                <button type="button" className='orderBtn'>Order Now</button>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Footer
--- src/components/Singlecard.jsx
+++ src/components/Singlecard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./Singlecard.css"
 
-function Singlecard() {
+function Singlecard(img, title, description, saleQuantity) {
+  
   return (
-    <div>card</div>
+    <>
+
+      <div className="cardBody">
+        <div className="cardImage"><img src={img} alt="" /></div>
+        <div className="cardData">
+          <p className="cardtitle">{title}</p>
+          <p className="cardDescription">{description}</p>
+          <p className="cardNumber">{saleQuantity}</p>
+
+        </div>
+
+      </div>
+
+
+
+    </>
   )
 }
 
